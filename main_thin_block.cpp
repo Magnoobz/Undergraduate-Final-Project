3eb1b570b1b4fe9daf2e2039435a00c880fdaa3d
Magnoobz <michael.agung67@gmail.com>
2022-03-31 08:58:04 +0700

    Thin Block

--- main_thin_block.cpp
+++ main_thin_block.cpp
@@ -19,80 +19,45 @@ int main()
 {
     auto start_time = chrono::high_resolution_clock::now();
 
+    double power    = 2909;
+    double radii    = 0.02;
+    double p_den    = power/(M_PI*radii*radii);
+    double T_init   = 23.7;
+    double k_init   = 0.016*T_init + 10.866;
+    double cp_init  = 435;
+    double rho_init = 8220;
+    double T_liq    = 1336;
+
     double x_left   = 0;
-    double x_right  = 0.1;
+    double x_right  = 0.1524;
     double y_bottom = 0;
-    double y_top    = 0.01;
-    double z_front  = 0.01;
+    double y_top    = 0.1524;
+    double z_front  = 0.0016;
     double z_back   = 0;
-
-    double x_change  = 0.3;
-    double x_change2 = 0.5;
-
-    double x_multires  = x_left+x_change*(x_right-x_left);
-    double x_multires2 = x_left+x_change2*(x_right-x_left);
 
     double eax = 1;
     double eay = 1;
-    double eaz = 1;
+    double eaz = 1/12.7;
 
-    // int n  = 40;
-    // int nx = n/eax + 1;
-    // int ny = n/eay + 1;
-    // int nz = n/eaz + 1;
+    double x_1 = x_left/eax;     // 0
+    double x_2 = x_right/eax;    // 0.1524
 
-    int nx = 100;
-    int ny = 10;
-    int nz = 10;
+    double y_1 = y_bottom/eay;   // 0
+    double y_2 = y_top/eay;      // 0.1524
 
-    // double ratio = 0.8;
+    double z_1 = z_back/eaz;     // 0
+    double z_2 = z_front/eaz;    // 0.02032
 
-    // int nx1 = x_change*nx;
-    // int nx2 = (1-x_change)*nx*ratio;
-    // int ny1 = ny;
-    // int ny2 = ny*ratio;
-    // int nz1 = nz;
-    // int nz2 = nz*ratio;
-
-    int nx1 = 30;
-    int nx2 = 16;
-    int nx3 = 30;
-
-    int ny1 = 10;
-    int ny2 = 8;
-    int ny3 = 6;
-
-    int nz1 = 10;
-    int nz2 = 8;
-    int nz3 = 6;
-
-    // nx += 1;
-    // ny += 1;
-    // nz += 1;
+    int nx = 120;
+    int ny = 120;
+    int nz = 16;
 
     double dx = (x_right-x_left)/(nx);
     double dy = (y_top-y_bottom)/(ny);
     double dz = (z_front-z_back)/(nz);
 
-    // double dx1 = (x_multires-x_left)/(nx1);
-    // double dy1 = (y_top-y_bottom)/(ny1);
-    // double dz1 = (z_front-z_back)/(nz1);
-
-    // double dx2 = (x_right-x_multires)/(nx2);
-    // double dy2 = (y_top-y_bottom)/(ny2);
-    // double dz2 = (z_front-z_back)/(nz2);
-
-    double dx1 = 0.001;
-    double dy1 = 0.001;
-    double dz1 = 0.001;
-
-    double dx2 = 0.00125;
-    double dy2 = 0.00125;
-    double dz2 = 0.00125;
-
-    double dx3 = 0.0016666;
-    double dy3 = 0.0016666;
-    double dz3 = 0.0016666;
+    double dx_out = 0.005;
+    double dx_in  = 0.008;
 
     vector<double> x, y, z, hx, hy, hz;
     vector<double> k, cp, rho;
@@ -103,36 +68,36 @@ int main()
     double n_dummy = 4;
     n_dummy += 0.5;
 
-    double x_par = x_left-n_dummy*dx1;
-    double y_par = y_bottom-n_dummy*dy1;
-    double z_par = z_back-n_dummy*dz1;
+    double x_par = x_left-n_dummy*dx;
+    double y_par = y_bottom-n_dummy*dy;
+    double z_par = z_back-n_dummy*dz;
 
-    for (int i = 0; i < nx1 + n_dummy - 0.5; i++)
+    for (int i = 0; i < nx + 2*n_dummy - 1; i++)
     {
-        x_par += dx1;
-        y_par = y_bottom-n_dummy*dy1;
+        x_par += dx;
+        y_par = y_bottom-n_dummy*dy;
 
-        for (int j = 0; j < ny1 + 2*n_dummy - 1; j++)
+        for (int j = 0; j < ny + 2*n_dummy - 1; j++)
         {
-            y_par += dy1;
-            z_par = z_back-n_dummy*dz1;
+            y_par += dy;
+            z_par = z_back-n_dummy*dz;
 
-            for (int m = 0; m < nz1 + 2*n_dummy - 1; m++)
+            for (int m = 0; m < nz + 2*n_dummy - 1; m++)
             {
-                z_par += dz1;
+                z_par += dz;
                 
                 x.push_back(x_par);
                 y.push_back(y_par);
                 z.push_back(z_par);
-                hx.push_back(dx1);
-                hy.push_back(dy1);
-                hz.push_back(dz1);
+                hx.push_back(dx);
+                hy.push_back(dy);
+                hz.push_back(dz);
 
-                if ((x_par<x_left+dx1) && (x_par>x_left))
+                if ((z_par<z_back+dz) && (z_par>z_back) && (pow(x_par-0.0762,2)+pow(y_par-0.0762,2) < pow(0.02,2)))
                 {
-                    q_x.push_back(1e5);
+                    q_x.push_back(0);
                     q_y.push_back(0);
-                    q_z.push_back(0);
+                    q_z.push_back(p_den);
                 }
                 else
                 {
@@ -141,109 +106,17 @@ int main()
                     q_z.push_back(0);
                 }
 
-                k.push_back(200);
-                cp.push_back(900);
-                rho.push_back(2700);
+                k.push_back(k_init);
+                cp.push_back(cp_init);
+                rho.push_back(rho_init);
 
                 x_w.push_back(eay*eaz*x_par);
                 y_w.push_back(eax*eaz*y_par);
                 z_w.push_back(eax*eay*z_par);
 
-                if ((x_par < x_left) || (x_par > x_right) || (y_par < y_bottom) || (y_par > y_top) || (z_par < z_back) || (z_par > z_front))
-                {
-                    is_dummy.push_back(1);
-                }
-                else
-                {
-                    is_dummy.push_back(0);
-                }
-            }
-        }
-    }
-
-    x_par = x_par + 0.5*dx1 - 0.5*dx2;
-
-    for (int i = 0; i < nx2; i++)
-    {
-        x_par += dx2;
-        y_par = y_bottom-n_dummy*dy2;
-
-        for (int j = 0; j < ny2 + 2*n_dummy - 1; j++)
-        {
-            y_par += dy2;
-            z_par = z_back-n_dummy*dz2;
-
-            for (int m = 0; m < nz2 + 2*n_dummy - 1; m++)
-            {
-                z_par += dz2;
-                
-                x.push_back(x_par);
-                y.push_back(y_par);
-                z.push_back(z_par);
-                hx.push_back(dx2);
-                hy.push_back(dy2);
-                hz.push_back(dz2);
-
-                q_x.push_back(0);
-                q_y.push_back(0);
-                q_z.push_back(0);
-                
-
-                k.push_back(200);
-                cp.push_back(900);
-                rho.push_back(2700);
-
-                x_w.push_back(eay*eaz*x_par);
-                y_w.push_back(eax*eaz*y_par);
-                z_w.push_back(eax*eay*z_par);
-
-                if ((x_par < x_left) || (x_par > x_right) || (y_par < y_bottom) || (y_par > y_top) || (z_par < z_back) || (z_par > z_front))
-                {
-                    is_dummy.push_back(1);
-                }
-                else
-                {
-                    is_dummy.push_back(0);
-                }
-            }
-        }
-    }
-
-    x_par = x_par + 0.5*dx2 - 0.5*dx3;
-
-    for (int i = 0; i < nx3 + n_dummy - 0.5; i++)
-    {
-        x_par += dx3;
-        y_par = y_bottom-n_dummy*dy3;
-
-        for (int j = 0; j < ny3 + 2*n_dummy - 1; j++)
-        {
-            y_par += dy3;
-            z_par = z_back-n_dummy*dz3;
-
-            for (int m = 0; m < nz3 + 2*n_dummy - 1; m++)
-            {
-                z_par += dz3;
-                
-                x.push_back(x_par);
-                y.push_back(y_par);
-                z.push_back(z_par);
-                hx.push_back(dx3);
-                hy.push_back(dy3);
-                hz.push_back(dz3);
-
-                q_x.push_back(0);
-                q_y.push_back(0);
-                q_z.push_back(0);
-                
-
-                k.push_back(200);
-                cp.push_back(900);
-                rho.push_back(2700);
-
-                x_w.push_back(eay*eaz*x_par);
-                y_w.push_back(eax*eaz*y_par);
-                z_w.push_back(eax*eay*z_par);
+                // x_w.push_back(x_par/eax);
+                // y_w.push_back(y_par/eay);
+                // z_w.push_back(z_par/eaz);
 
                 if ((x_par < x_left) || (x_par > x_right) || (y_par < y_bottom) || (y_par > y_top) || (z_par < z_back) || (z_par > z_front))
                 {
@@ -268,35 +141,13 @@ int main()
 
     for (int i = 0; i < num_particle; i++)
     {
-    //     if (x[i] < 0)
-    //     { xi == 0; }
-    //     else if (x[i] > 1)
-    //     { xi == 1; }
-    //     else
-    //     { xi = x[i]; }
-
-    //     if (y[i] < 0)
-    //     { yi == 0; }
-    //     else if (y[i] > 1)
-    //     { yi == 1; }
-    //     else
-    //     { yi = y[i]; }
-
-    //     if (z[i] < 0)
-    //     { zi == 0; }
-    //     else if (z[i] > 1)
-    //     { zi == 1; }
-    //     else
-    //     { zi = z[i]; }
-
-        T[i] = 27;
+        T[i] = T_init;
     }
 
     auto start_neighbor_search = chrono::high_resolution_clock::now();
 
-    // double R_e = 2.4;
+    double R_e = 2.4;
     // brute_force_3D(x_w, y_w, z_w, hx, eay, eaz, neighbor, weight_data, R_e);
-    double R_e = 2.9;
     brute_force_3D_2(x_w, y_w, z_w, hx, eay, eaz, neighbor, weight_data, R_e);
 
     auto end_neighbor_search = chrono::high_resolution_clock::now();
@@ -339,60 +190,34 @@ int main()
 
     int count = 0;
     double t  = 0;
-    double dt = 5e-3;
-    
-    string name1 = "output/3D External Flux/Multires/result 86/big/out_" + to_string(count) + ".csv";
-    string name2 = "output/3D External Flux/Multires/result 86/small/out_" + to_string(count) + ".csv";
-    string name3 = "output/3D External Flux/Multires/result 86/smaller/out_" + to_string(count) + ".csv";
+    double dt = 1e-2;
 
-    ofstream output1, output2, output3;
+    string name = "output/Thin Block/result/out_" + to_string(count) + ".csv";
 
-    output1.open(name1);
-    output2.open(name2);
-    output3.open(name3);
+    ofstream output1;
 
-    output1 << "x" << "," << "y" << "," << "z" << "," << "qx" << "," << "LSMPS_Conserved\n";
-    output2 << "x" << "," << "y" << "," << "z" << "," << "qx" << "," << "LSMPS_Conserved\n";
-    output3 << "x" << "," << "y" << "," << "z" << "," << "qx" << "," << "LSMPS_Conserved\n";
+    output1.open(name);
+
+    output1 << "x" << "," << "y" << "," << "z" << "," << "q" << "," << "LSMPS_Conserved\n";
 
     for (int i = 0; i < num_particle; i++)
     {
         if (x[i] >= x_left && x[i] <= x_right && y[i] >= y_bottom && y[i] <= y_top && z[i] >= z_back && z[i] <= z_front)
         {
-            if (hx[i] == dx1)
-            {
-                output3 << x[i] << "," 
-                    << y[i] << ","
-                    << z[i] << ","
-                    << q_x[i] << ","
-                    << T[i] << "\n";
-            }
-            else if (hx[i] == dx2)
-            {
-                output2 << x[i] << "," 
-                    << y[i] << ","
-                    << z[i] << ","
-                    << q_x[i] << ","
-                    << T[i] << "\n";
-            }
-            else
-            {
-                output1 << x[i] << "," 
-                    << y[i] << ","
-                    << z[i] << ","
-                    << q_x[i] << ","
-                    << T[i] << "\n";
-            }
-            
+            output1 << x_w[i] << "," 
+                << y_w[i] << ","
+                << z_w[i] << ","
+                << q_x[i] << ","
+                << T[i] << "\n";
         }
     }
 
     auto start_loop_segment = chrono::high_resolution_clock::now();
     auto end_loop_segment = chrono::high_resolution_clock::now();
 
-    while (t < 100 /*count < 1*/)
+    while (t < 15 /*count < 1*/)
     {
-        if (count % 100 == 0)
+        if (count % 10 == 0)
         {
             start_loop_segment = chrono::high_resolution_clock::now();
         }
@@ -444,10 +269,21 @@ int main()
         // double int_time_ms = std::chrono::duration_cast <std::chrono::milliseconds> (end_int-end_dtdt).count();
         // printf("Time Integration Time   : %f second\n", int_time_ms/1000);
 
+        k.clear();
+        # pragma omp parallel for
+        for (int i = 0; i < num_particle; i++)
+        {
+            if (T[i] > 1336)
+            {
+                T[i] = 1336;
+            }
+            k.push_back(0.016*T[i] + 10.866);
+        }
+
         t += dt;
         count += 1;
 
-        if (count % 100 == 0)
+        if (count % 10 == 0)
         {
             end_loop_segment = chrono::high_resolution_clock::now();
             double loop_time_ms = std::chrono::duration_cast <std::chrono::milliseconds> (end_loop_segment-start_loop_segment).count();
@@ -455,51 +291,25 @@ int main()
             cout << count <<":"<<"\t"<< t << "\t\t Segment time: " << loop_time_ms/1000 << endl;
         }
         
-        if (count % 500 == 0)
+        if (count % 10 == 0)
         {
-            string name1 = "output/3D External Flux/Multires/result 86/big/out_" + to_string(count) + ".csv";
-            string name2 = "output/3D External Flux/Multires/result 86/small/out_" + to_string(count) + ".csv";
-            string name3 = "output/3D External Flux/Multires/result 86/smaller/out_" + to_string(count) + ".csv";
+            string name = "output/Thin Block/result/out_" + to_string(count) + ".csv";
 
-            ofstream output1, output2, output3;
+            ofstream output1;
 
-            output1.open(name1);
-            output2.open(name2);
-            output3.open(name3);
+            output1.open(name);
 
-            output1 << "x" << "," << "y" << "," << "z" << "," << "qx" << "," << "LSMPS_Conserved\n";
-            output2 << "x" << "," << "y" << "," << "z" << "," << "qx" << "," << "LSMPS_Conserved\n";
-            output3 << "x" << "," << "y" << "," << "z" << "," << "qx" << "," << "LSMPS_Conserved\n";
+            output1 << "x" << "," << "y" << "," << "z" << "," << "q" << "," << "LSMPS_Conserved\n";
 
             for (int i = 0; i < num_particle; i++)
             {
                 if (x[i] >= x_left && x[i] <= x_right && y[i] >= y_bottom && y[i] <= y_top && z[i] >= z_back && z[i] <= z_front)
                 {
-                    if (hx[i] == dx1)
-                    {
-                        output3 << x[i] << "," 
-                            << y[i] << ","
-                            << z[i] << ","
-                            << q_x[i] << ","
-                            << T[i] << "\n";
-                    }
-                    else if (hx[i] == dx2)
-                    {
-                        output2 << x[i] << "," 
-                            << y[i] << ","
-                            << z[i] << ","
-                            << q_x[i] << ","
-                            << T[i] << "\n";
-                    }
-                    else
-                    {
-                        output1 << x[i] << "," 
-                            << y[i] << ","
-                            << z[i] << ","
-                            << q_x[i] << ","
-                            << T[i] << "\n";
-                    }
-                    
+                    output1 << x_w[i] << "," 
+                        << y_w[i] << ","
+                        << z_w[i] << ","
+                        << q_z[i] << ","
+                        << T[i] << "\n";
                 }
             }
         }
@@ -516,11 +326,10 @@ int main()
     printf("Sij Star Time               : %f second\n", sijstar_time_ms/1000);
     printf("Calculation Time            : %f second\n\n", calc_time_ms/1000);
 
-
-    ofstream output4;
-    output4.open("output/3D External Flux/Multires/result 86/summary.csv");
+    ofstream output2;
+    output2.open("output/Thin Block/result/summary.csv");
     
-    output4  << "Number of Particle," << x.size() <<"\n"
+    output2  << "Number of Particle," << x.size() <<"\n"
             << "Neighbor Search Time," << neighbor_time_ms/1000 << "\n"
             << "Calc Eta Time," << eta_time_ms/1000 << "\n"
             << "Sij Time," << sij_time_ms/1000 << "\n"
